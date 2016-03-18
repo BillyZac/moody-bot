168f863d04b1c6f7ea00c418682cfa3e6b78e89e
Zachary Smith <billyzacsmith@gmail.com>
2016-03-18 15:10:48 -0600

    Clean up mention response logic.

--- bot.js
+++ bot.js
@@ -19,91 +19,81 @@ var T = new Twit({
  timeout_ms: 60*1000
 })
 
-T.get('statuses/mentions_timeline', function(err, mentions) {
-  if (err) { console.log('There was an error getting the mentions', err) }
+if (online === 'online') {
+  T.get('statuses/mentions_timeline', function(err, mentions) {
+    if (err) { console.log('There was an error getting the mentions', err) }
 
-  mentions.forEach(function(mention) {
-    console.log('Got a mention:', mention.text)
+    mentions.forEach(function(mention) {
+      console.log('Got a mention:', mention.text)
 
-    responseCollection.find({ "original_mention_id": mention.id }).then(function(result) {
-      if (result.length > 0) {
-        console.log('Mention is in the DB, so not responding:');
-        console.log(mention.id);
-      } else {
-        console.log('Mention is not in the DB.');
-        if (online === 'online') {
-          console.log('Going to respond', 'Online:', online);
+      responseCollection
+      .find({ "original_mention_id": mention.id })
+      .then(function(result) {
+        // If the mention has not already received a response, respond
+        if (result.length === 0) {
+          console.log('Going to respond to', mention.text);
           respond(mention)
         } else {
-          console.log('Not going to respond', 'Online:', online);
+          console.log('Not to responding to', mention.text);
         }
-      }
+      })
     })
   })
-})
-
+}
 
 function respond(mention) {
-  // Fake the input for the image generation.
-  // This will eventually come from the personality profile data.
-  var frequency = Math.random() * 100
-
   unirest.get('https://personality.herokuapp.com/:' + mention.user.screen_name)
   .end(function(response) {
     var personality = response.body
-    // console.log(personality.traits[0].children[2].name)
     var emotionality = Math.trunc(parseFloat(personality.traits[0].children[2].percentage) * 10)
-    console.log(emotionality);
+
     // Make the image
-    makeDrawing(frequency, 100)
+    makeDrawing(100, 100)
     .then(function(filePath) {
+      // Read the image file
+      var b64content = fs.readFileSync(filePath, { encoding: 'base64' })
 
-        console.log('Finished making the drawing:', filePath);
+      // Upload the image to Twitter
+      T.post('media/upload', { media_data: b64content }, function (err, data, response) {
 
-        // Read the image file
-        var b64content = fs.readFileSync(filePath, { encoding: 'base64' })
+        // Post a tweet with a reference to the image file
+        var mediaIdStr = data.media_id_string
 
-        // Upload the image to Twitter
-        T.post('media/upload', { media_data: b64content }, function (err, data, response) {
-
-          // Post a tweet with a reference to the image file
-          var mediaIdStr = data.media_id_string
-
-          // Construct a message to the mentioner
-          // Using #user instead of @user to avoid violating Twitter's TOS
-          var status =
-          [
-            '#',
-            mention.user.screen_name,
-            ' ',
-            'I\'d rate you a ',
-            emotionality,
-            ' out of ten in terms of emotionality.'
-          ].join('')
+        // Construct a message to the mentioner
+        // Using #user instead of @user to avoid violating Twitter's TOS
+        var status =
+        [
+          '#',
+          mention.user.screen_name,
+          ' ',
+          'You\'re a ',
+          emotionality,
+          ' out of ten in terms of emotionality.'
+        ].join('')
 
 
-          var params = {
-            status: status,
-            // media_ids: [mediaIdStr]
-          }
+        var params = {
+          status: status,
+          // media_ids: [mediaIdStr]
+        }
 
-          params.status = 'Little cows make puppy chow.'
+        // params.status = 'Every thumb is my chum.'
 
-          T.post('statuses/update', params, function (error, data, response) {
-            if (error) {
-              console.log('There was an error posting to Twitter:', error)
-            } else {
-              console.log('Successfully posted.')
-              var responseToMention = {
-                original_mention_id: mention.id,
-                response_id: data.id
-              }
-              console.log('To save to DB:', responseToMention);
-              responseCollection.insert(responseToMention)
+        T.post('statuses/update', params, function (error, data, response) {
+          if (error) {
+            console.log('There was an error posting to Twitter:', error)
+          } else {
+            console.log('Successfully posted.')
+            var responseToMention = {
+              original_mention_id: mention.id,
+              response_id: data.id
             }
-            db.close()
-          })
+            console.log('To save to DB:', responseToMention);
+            responseCollection.insert(responseToMention)
+          }
+          db.close()
         })
+      })
     })
   })
 }
